ed5f51d205458235ccd9118e22b598e422053e21
akshayshinde97 <kameshshinde27@gmail.com>
2021-12-08 23:28:04 +0530

    optimized counter project.

--- counter/app.js
+++ counter/app.js
@@ -3,26 +3,49 @@ const decbtn = document.getElementById("dec");
 const incbtn = document.getElementById("inc");
 const cntr = document.querySelector("#value");
 const resbtn = document.getElementById("res");
-
-decbtn.addEventListener("click", function(){
-    count -=1;
-    cntr.textContent = count;
-    cntr.classList = "neg";
-});
-resbtn.addEventListener("click", function(){
-    if(count < 0){
-        cntr.classList.remove("neg");
-    }
-    else if(count > 0)
-    {
-        cntr.classList.remove("pos");
-    }
-    count = 0;
-    cntr.textContent = count;
+// my Approach
+// decbtn.addEventListener("click", function(){
+//     count -=1;
+//     cntr.textContent = count;
+//     cntr.classList = "neg";
+// });
+// resbtn.addEventListener("click", function(){
+//     if(count < 0){
+//         cntr.classList.remove("neg");
+//     }
+//     else if(count > 0)
+//     {
+//         cntr.classList.remove("pos");
+//     }
+//     count = 0;
+//     cntr.textContent = count;
     
-});
-incbtn.addEventListener("click", function(){
-    count += 1;
-    cntr.textContent = count;
-    cntr.classList = "pos";
+// });
+// incbtn.addEventListener("click", function(){
+//     count += 1;
+//     cntr.textContent = count;
+//     cntr.classList = "pos";
+// });
+// Effective Approach
+const btns = document.querySelectorAll(".btn");
+btns.forEach(function (btn){
+    btn.addEventListener('click',function(eve){
+        const styclass = eve.currentTarget.classList;
+        if(styclass.contains('decrease')){
+            count -= 1;
+        }
+        else if(styclass.contains('increase')){
+            count +=1;
+        }
+        else if(styclass.contains('reset')){
+            count = 0;
+        }
+        if(count > 0){
+            cntr.style.color = "green";
+        }
+        else if(count < 0){
+            cntr.style.color = "red";
+        }
+        cntr.textContent = count;
+    });
 });
---
+++ counter/learning.md
@@ -0,0 +1,16 @@
+# Learnings:-
+>Applying eventlistner on Nodelist using foreach method .
+
+>currentTarget :Get the element whose event listeners triggered a specific event.
+
+>Event.currentTarget is interesting to use when attaching the same event handler to several elements.
+
+>The Element.classList is a read-only property that returns a live DOMTokenList collection of the class attributes of the element.
+
+>Although the classList property itself is read-only, you can modify its associated DOMTokenList using the add(), remove(), replace(), and toggle() methods.
+
+>The contains() method of the DOMTokenList interface returns a boolean value — true if the underlying list contains the given token, otherwise false.
+
+> More about markdown [https://github.com/jamesqquick/markdown-worksheet/blob/master/worksheet.md#emphasis]()
+
+> ctrl + K V for preview of markdown.
